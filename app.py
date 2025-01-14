9920d3931be3cc289f34166a05d4f08951a6c4b1
rongfengliang <1141591465@qq.com>
2025-01-14 10:51:01 +0800

    add more

--- app.py
+++ app.py
@@ -8,10 +8,59 @@ def get_data_layer():
     return SQLAlchemyDataLayer(conninfo="sqlite+aiosqlite:///database.db",storage_provider=S3StorageClient("demo"))
 
 
+@cl.set_chat_profiles
+async def chat_profile(current_user: cl.User):
+    if current_user.metadata["role"] != "ADMIN":
+        return None
+
+    return [
+        cl.ChatProfile(
+            name="My Chat Profile",
+            icon="https://picsum.photos/250",
+            markdown_description="The underlying LLM model is **GPT-3.5**, a *175B parameter model* trained on 410GB of text data.",
+            starters=[
+                cl.Starter(
+                    label="Morning routine ideation",
+                    message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
+                    icon="/public/logo_light.png",
+                ),
+                cl.Starter(
+                    label="Explain superconductors",
+                    message="Explain superconductors like I'm five years old.",
+                    icon="/public/logo_light.png",
+                ),
+            ],
+        )
+    ]
+
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="Morning routine ideation",
+            message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
+            icon="/public/logo_light.png",
+            ),
+
+        cl.Starter(
+            label="Explain superconductors",
+            message="Explain superconductors like I'm five years old.",
+            icon="/public/logo_light.png",
+            ),
+        cl.Starter(
+            label="Python script for daily email reports",
+            message="Write a script to automate sending daily email reports in Python, and walk me through how I would set it up.",
+            icon="/public/logo_light.png",
+            ),
+        cl.Starter(
+            label="Text inviting friend to wedding",
+            message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
+            icon="/public/logo_light.png",
+            )
+        ]
+
 @cl.password_auth_callback
 def auth_callback(username: str, password: str):
-    # Fetch the user matching username from your database
-    # and compare the hashed password with the value stored in the database
     if (username, password) == ("admin", "admin"):
         return cl.User(
             identifier="admin", metadata={"role": "admin", "provider": "credentials"}
@@ -21,9 +70,6 @@ def auth_callback(username: str, password: str):
     
 @cl.on_message
 async def main(message: cl.Message):
-    # Your custom logic goes here...
-
-    # Send a response back to the user
     await cl.Message(
         content=f"Received: {message.content}",
     ).send()
